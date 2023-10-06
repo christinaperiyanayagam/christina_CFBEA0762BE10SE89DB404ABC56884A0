d1d8ebb34fa15a7eb3e44140e210682e8ac729e0
christinaperiyanayagam <145101376+christinaperiyanayagam@users.noreply.github.com>
2023-10-06 11:01:36 +0530

    main (4).py

---
+++ main (4).py
@@ -0,0 +1,20 @@
+
+
+def linearsearchproduc(productList, targetproduct):
+  indices = []
+  
+
+for index ,product in enumerate(produtList):
+  if product == targetproduct:
+    indices.append(index)
+
+    
+
+
+    
+
+products = ["shoes",",boot","loafer","shoes","sandal","shoes"]
+target = "shoes"
+result = linearsearchproduct(products,target)
+print(result)
+                  
